10e554c4efe3cf0f50d145333638fc40359c29da
Tyler Tackett <ttackett412@yahoo.com>
2017-09-28 14:48:39 -0400

    Committing the progress of the project

--- main.cpp
+++ main.cpp
@@ -6,17 +6,22 @@ using namespace std;
 class ShopItemOrder {
 public:
     
-    ShopItemOrder()
+    //constructor that asks for needed information
+    ShopItemOrder(string Name, double itemsOrdered, double Price)
     {
-        
+        this->name = Name;
+        this->itemsOrdered = itemsOrdered;
+        this->price = Price;
     }
     
+    
+    //ways to get and reset different variables
     void setName(string Name){
         this->name = Name; 
     }
     
     string getName(){
-           return name; 
+        return name; 
     }
     
     void setPrice(double Price){
@@ -27,40 +32,50 @@ public:
         return price;  
     }
     
-    void setOrders(int Items){;
+    void setOrders(double Items){;
         this->itemsOrdered = Items;
     }
     
-    int getOrders(){
+    double getOrders(){
         return itemsOrdered; 
     }
     
-    void thankYou(){
-        cout << "You're order of " << name << " cost " << price << std::endl;
-        cout << "You're total price is " << totalPrice;
-    }
-    
-    double total()
-    {
-       totalPrice = totalPrice + price; 
-    }
-    
 private:
-    double totalPrice;
-    int itemsOrdered;
+    double itemsOrdered;
     double price;
     string name;
 };
 
 int main(void) {
-    ShopItemOrder Order1;
     
-    Order1.setOrders(1);
-    Order1.setName("Pickle");
-    Order1.setPrice(1.15);
-    Order1.total();
+    //creates the orders
+    ShopItemOrder Order1("Books", 2, 2.00);
+    ShopItemOrder Order2("Pens", 5, 1.00);
+    ShopItemOrder Order3("Waters", 3, 1.00);
     
-    Order1.thankYou();
+    //adds the orders to a vector
+    ShopItemOrder Orders[3] = {Order1, Order2, Order3};
     
+    //size of the vector
+    int n = sizeof(Orders) / sizeof(Orders[0]); 
+    //holds all the total prices for in individual orders
+    int pricePerOrder[3];
+    //holds the total prices for everything
+    int totalPrice = 0;
+    
+    //gets the prices
+    for(int i = 0; i < 3; i++)
+    {
+        pricePerOrder[i] = Orders[i].getPrice() * Orders[i].getOrders();
+        totalPrice = totalPrice + pricePerOrder[i];
+    }
+    
+    //outputs all order information
+    for(int i = 0; i < n; i++)
+    {
+        cout << "You're order of " << Orders[i].getOrders() << " " << Orders[i].getName() << " costs $" << pricePerOrder[i] << endl;
+    }
+    
+    cout << "The total price of all your orders are $" << totalPrice;
 }
 
